ac9ef2f8b86a3e9941edb5fda9a04c96b72acf62
Punit <3033.punit@gmail.com>
2025-05-26 10:08:08 +0530

    feat: self api call in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getUserData = async () => {
   const response = await axios.get(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+    "http://localhost:3000/api/user"
   );
   return await response.data;
 };
