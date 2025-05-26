91d37990c4e18999c4f40a9f2d15e096b7dce411
Punit <3033.punit@gmail.com>
2025-05-26 10:06:06 +0530

    feat: axios backend call simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <div>hi there</div>
+const getUserData = async () => {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
   );
+  return await response.data;
+};
+
+//async component only in NEXTJS and only in server side components not client side
+export default async function Home() {
+  const userDetails = await getUserData();
+
+  return <div>hi there
+    {userDetails.email}
+    {userDetails.name}
+  </div>;
 }
